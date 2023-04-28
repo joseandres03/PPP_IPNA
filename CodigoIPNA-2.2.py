857a35955faa2c16c899a8bbf7d8a0b60a1850e6
joseandres03 <alu0101232201@ull.edu.es>
2023-04-28 11:53:06 +0100

    Version 2.2

--- CodigoIPNA-2.2.py
+++ CodigoIPNA-2.2.py
@@ -15,7 +15,6 @@ import matplotlib.dates as mdates
 from matplotlib import ticker
 import glob
 
-
 # Archivos fijos para todas las medidas
 satpcv_file = "C:\IPNA\data\ANTEX\igs20.atx"
 blq_folder = "C:\IPNA\data\OTL_BLQ"
@@ -65,7 +64,7 @@ def select_antex_file(antex_folder):
             except ValueError:
                 print("Entrada inválida. Intente de nuevo.")
 
-# Solicitar al usuario que seleccione el archivo ANTEX de su antena receptora
+# Solicitar al usuario que seleccione el archivo ANTEX de su antena receptora (si solo hay un archivo usará automaticamente ese)
 antex_file = select_antex_file(antex_folder)
 
 # Seleccion del archivo .blq.
@@ -90,45 +89,37 @@ def select_otl_file(otl_folder):
             except ValueError:
                 print("Entrada inválida. Intente de nuevo.")
 
-# Solicitar al usuario que seleccione el archivo .blq de la ubicacion de interes
+# Solicitar al usuario que seleccione el archivo .blq de la ubicacion de interes (si solo hay un archivo usará automaticamente ese)
 blq_file = select_otl_file(blq_folder)
 
-
-# Itera sobre todos los archivos en la carpeta de entrada
+# Itera sobre todos los archivos crudos en la carpeta de entrada.
 for filename in os.listdir(input_folder):
+    
     # Funcion para procesar los archivos septentrio y obtener los rinex (.o y .n)
     def convert_septentrio_to_rinex(input_folder):
-        
         # Crea las subcarpetas para los archivos de salida (.o y .n) si no existen
         base_folder = os.path.dirname(input_folder)
         obs_output_folder = os.path.join(base_folder, 'OBS')
         if not os.path.exists(obs_output_folder):
             os.makedirs(obs_output_folder)
-
         nav_output_folder = os.path.join(base_folder, 'NAV')
         if not os.path.exists(nav_output_folder):
             os.makedirs(nav_output_folder)
-        
         # Divide el nombre del archivo en su nombre base y su extensión
         file_base, file_ext = os.path.splitext(filename)
-        
         # Construye las rutas completas a los archivos de entrada y salida
         input_file = os.path.join(input_folder, filename)
         obs_output_file = os.path.join(obs_output_folder, file_base + '.o')
         nav_output_file = os.path.join(nav_output_folder, file_base + '.n')
-
         # Construye el comando Teqc
         teqc_command = [os.path.join(teqc_executables_path, teqc_executable), '+obs', obs_output_file, '+nav', nav_output_file, input_file]
-
         # Ejecuta el comando Teqc
         subprocess.run(teqc_command)
-        
         # Devuelve el nombre base del archivo para su posterior utilizacion.    
         return file_base
     
     # Ejecutar la función convert_septentrio_to_rinex y obtener el nombre base del archivo Septentrio
     septentrio_filename_base = convert_septentrio_to_rinex(input_folder)
-    
     # Encontrar el archivo .o en la carpeta de archivos de observación y el archivo .n en la de navegación
     observation_file = os.path.join('C:\IPNA\data', 'OBS', f"{septentrio_filename_base}.o")
     navigation_file = os.path.join('C:\IPNA\data', 'NAV', f"{septentrio_filename_base}.n")
@@ -156,16 +147,13 @@ for filename in os.listdir(input_folder):
         # Fecha de referencia del GPS (1980-01-06 00:00:00)
         gps_epoch = datetime(1980, 1, 6)
         delta = dt - gps_epoch
-
         # Calcular la semana y el día GPS
         gps_week = delta.days // 7
         gps_day = delta.days % 7
-
         return gps_week, gps_day
     
     # Leer la fecha de la observación
     date_observation = get_date_from_rinex(observation_file)
-    
     # Tiempo en calendario GPS
     gps_week, gps_day = datetime_to_gps_week_day(date_observation)
     print(f"Semana GPS: {gps_week}, Día GPS: {gps_day}")
@@ -176,19 +164,16 @@ for filename in os.listdir(input_folder):
         if os.path.exists(output_file):
             print(f"El archivo {output_file} ya existe en la carpeta de destino. Omitiendo la descarga.")
             return output_file
-
         try:
             print("Descargando el archivo: {}".format(url))
             response = requests.get(url, stream=True)
             response.raise_for_status()  # Lanza una excepción si hay algún error HTTP
-
             with open(compressed_file, "wb") as f:
                 for chunk in response.iter_content(chunk_size=8192):
                     f.write(chunk)
         except Exception as e:
             print("Error al descargar el archivo: {} - Error: {}".format(url, e))
             return None
-
         # Descomprimir el archivo
         if platform.system() == "Windows":
             subprocess.run(['WinRAR.exe', 'e', '-y', compressed_file], cwd=output_folder)
@@ -196,11 +181,9 @@ for filename in os.listdir(input_folder):
             subprocess.run(['uncompress', compressed_file], cwd=output_folder)
         else:
             raise Exception('Sistema operativo no soportado')
-
         # Renombrar el archivo descomprimido y eliminar el archivo comprimido
         os.rename(os.path.join(output_folder, os.path.basename(compressed_file[:-2])), output_file)
         os.remove(compressed_file)
-
         return output_file
     
     # Función para descargar los archivos gnss necesarios para su posterior uso en RTKPOST
@@ -210,59 +193,55 @@ for filename in os.listdir(input_folder):
         # Calcular el día del año y el trimestre correspondiente
         day_of_year = get_day_of_year(date_observation)
         quarter, quarter_start_day = get_quarter(day_of_year)
-        print(f"Día del año: {day_of_year}, Trimestre: {quarter}")
         
+        print(f"Día del año: {day_of_year}, Trimestre: {quarter}")
         # Crear las carpetas CLOCK, ERP, DCB, IONEX y SP3 si no existen
         sp3_output_folder = os.path.join('C:\IPNA\data', 'SP3')
         if not os.path.exists(sp3_output_folder):
             os.makedirs(sp3_output_folder)
-            
         cls_output_folder = os.path.join('C:\IPNA\data', 'CLOCK')
         if not os.path.exists(cls_output_folder):
             os.makedirs(cls_output_folder)
-
         erp_output_folder = os.path.join('C:\IPNA\data', 'ERP')
         if not os.path.exists(erp_output_folder):
             os.makedirs(erp_output_folder)
-        
         dcb_output_folder = os.path.join('C:\IPNA\data', 'DCB')
         if not os.path.exists(dcb_output_folder):
             os.makedirs(dcb_output_folder)
-        
         ionex_output_folder = os.path.join('C:\IPNA\data', 'IONEX')
         if not os.path.exists(ionex_output_folder):
             os.makedirs(ionex_output_folder)
-        
+            
         # Construir el nombre del archivo de salida sp3 utilizando el nombre base del archivo Septentrio
-        sp3_output_filename = "{}.sp3".format(septentrio_filename_base)  
-
+        sp3_output_filename = "{}.sp3".format(septentrio_filename_base)
+        
         # Descargar y descomprimir archivo sp3
         sp3_url_path = "/archive/gnss/products/{}/igs{}{}.sp3.Z".format(gps_week, gps_week, gps_day)
         sp3_url = url_base + sp3_url_path
         sp3_compressed_file = os.path.join(sp3_output_folder, "igs{}{}.sp3.Z".format(gps_week, gps_day))
         sp3_uncompressed_file = os.path.join(sp3_output_folder, sp3_output_filename)
         download_and_uncompress_file(sp3_url, sp3_output_folder, sp3_uncompressed_file, sp3_compressed_file)
-
+        
         # Descargar y descomprimir archivo .cls
         cls_url_path = "/archive/gnss/products/{}/igs{}{}.cls.Z".format(gps_week, gps_week, gps_day)
         cls_url = url_base + cls_url_path
         cls_compressed_file = os.path.join(cls_output_folder, "{}.cls.Z".format(septentrio_filename_base))
         cls_uncompressed_file = os.path.join(cls_output_folder, "{}.cls".format(septentrio_filename_base))
         download_and_uncompress_file(cls_url, cls_output_folder, cls_uncompressed_file, cls_compressed_file)
-
+        
         # Descargar y descomprimir archivo .erp
         erp_url_path = "/archive/gnss/products/{}/igs{}7.erp.Z".format(gps_week, gps_week)
         erp_url = url_base + erp_url_path
         erp_compressed_file = os.path.join(erp_output_folder, "{}.erp.Z".format(gps_week))
         erp_uncompressed_file = os.path.join(erp_output_folder, "{}.erp".format(gps_week))
         download_and_uncompress_file(erp_url, erp_output_folder, erp_uncompressed_file, erp_compressed_file)
-
+        
         # Descargar y descomprimir el archivo DCB
         dcb_output_file = os.path.join(dcb_output_folder, f"{quarter}_trimestre.dcb")
         dcb_url = "https://cddis.nasa.gov/archive/gnss/products/bias/{}/DLR0MGXFIN_{}{}0000_03L_01D_DCB.BSX.gz".format(date_observation.year, date_observation.year, quarter_start_day)
         compressed_dcb_file = os.path.join(dcb_output_folder, "DLR0MGXFIN_{}{}0000_03L_01D_DCB.BSX.gz".format(date_observation.year, quarter_start_day))
         download_and_uncompress_file(dcb_url, dcb_output_folder, dcb_output_file, compressed_dcb_file)
-
+        
         # Descargar y descomprimir el archivo de ionosfera
         two_digit_year = date_observation.year % 100
         day_of_year = get_day_of_year(date_observation)
@@ -280,7 +259,7 @@ for filename in os.listdir(input_folder):
     def update_rtkpost_conf(rtkpost_conf, satpcv_file, antex_file, ionex_file, dcb_file, erp_file, blq_file):
         with open(rtkpost_conf, 'r') as f:
             content = f.read()
-
+            
         content = content.replace('file-satantfile    = ', f'file-satantfile    ={satpcv_file}')
         content = content.replace('file-rcvantfile    = ', f'file-rcvantfile    ={antex_file}')
         content = content.replace('file-ionofile      = ', f'file-ionofile      ={ionex_file}')
@@ -301,27 +280,30 @@ for filename in os.listdir(input_folder):
     
     # Ruta y nombre del archivo de salida que se obtendrá al ejecutar rtkpost
     output_file = os.path.join(r"C:\\IPNA\\data\\POST", f"{septentrio_filename_base}.pos")
-    
     # Ejecucion de rtkpost
     run_rtkpost(rtkpost_exe, rtkpost_conf, observation_file, navigation_file, sp3_file, cls_file, output_file)
     
-
     # Obtencion de un valor promedio para cada coordenada en ese día
+    # Funcion para convertir de grados minutos y segundos a solo grados
     def dms2dd(d, m, s):
          return d + m / 60 + s / 3600
     
+    # Funcion para pasar de grados a grados minutos y segundos
     def dd2dms(coord):
         degrees = int(coord)
         minutes = int((coord - degrees) * 60)
         seconds = (coord - degrees - minutes / 60) * 3600
         return degrees, minutes, seconds
     
+    # Esta funcion calcula la velocidad a la que cambian los datos de coordenadas
     def velocidad(valores):
         return [valores[i+1] - valores[i] for i in range(len(valores) - 1)]
-
+    
+    # Funcion que calcula el promedio de una cantidad de valores
     def promedio(valores):
         return sum(valores) / len(valores)
-
+    
+    # Funcion que selecciona los valores a promediar una vez estos se estabilizan.
     def estabilizacion_modificada(valores, umbral_velocidad):
         vels = velocidad(valores)
         ultimo_indice_superado = None
@@ -363,7 +345,8 @@ for filename in os.listdir(input_folder):
         ax[subplot_idx].xaxis.set_minor_formatter(mdates.DateFormatter('%H:%M'))
         ax[subplot_idx].tick_params(axis='x', which='minor', bottom=True)
         ax[subplot_idx].legend()
-
+    
+    # Funciones para modificar el formato de escala los valores
     def coord_tick_formatter(x, pos):
         d, m, s = dd2dms(x)
         return f"{d:.0f}°{m:.0f}'{s:.2f}\""
@@ -373,20 +356,22 @@ for filename in os.listdir(input_folder):
 
     coord_ticks = ticker.FuncFormatter(coord_tick_formatter)
     altitude_ticks = ticker.FuncFormatter(altitude_tick_formatter)
-
+    
+    # Ruta al archivo .pos obtenido con rtkpost para calcular el valor promedio para ese dia
     position_file = os.path.join('C:\IPNA\data', 'POST', f"{septentrio_filename_base}.pos")
+    # Ruta a la carpeta donde se guardará el archivo con los promedios
     ruta_resultados = "C:\\IPNA\\data\\your_results"
-
+    
+    # Umbrales de velocidad angular y metrica para establecer un valor minimo de estabilizacion de los datos a promediar
     umbral_angular = 4e-08
     umbral_metrico = 0.015
-
+    
+    # Abrir el archivo pos para su lectura y procesamiento de datos con las anteriores funciones.
     with open(position_file, "r") as archivo_pos:
         lines = archivo_pos.readlines()
-        
         # Extraigo los datos latitudes, longitudes y alturas y los meto en listas
         latitudes, longitudes, alturas = [], [], []
         tiempos = []
-        
         # Obtengo los valores de desviacion estandar y error de la ultima medida del día y la tomo como error y desviacion del día
         sdn, sde, sdu, sdne, sdeu, sdue = 0, 0, 0, 0, 0, 0
         for line in reversed(lines):
@@ -395,30 +380,27 @@ for filename in os.listdir(input_folder):
                 if len(data) == 19:  # Hay 20 elementos en lugar de 18 debido a la estructura de latitud y longitud
                     sdn, sde, sdu, sdne, sdeu, sdue = map(float, data[11:17])  # Cambiar aquí el índice inicial a 14 y final a 19
                     break
-
+        # Guardo los valores de latitudes, longitudes y altura en las listas definidas al comienzo.
         for line in lines:
             if line.strip() and not line.startswith('%'):
                 data = line.split()
                 fecha, hora, *coords = data
                 lat_d, lat_m, lat_s, lon_d, lon_m, lon_s, alt, *_ = map(float, coords)
-
                 lat = dms2dd(lat_d, lat_m, lat_s)
                 lon = dms2dd(lon_d, lon_m, lon_s)
-
                 latitudes.append(lat)
                 longitudes.append(lon)
                 alturas.append(alt)
                 tiempos.append(datetime.strptime(f'{fecha} {hora}', '%Y/%m/%d %H:%M:%S.%f'))
-
+        # Calculo con el promedio de cada una de las listas una vez esten estabilizados.
         promedio_lat = estabilizacion_modificada(latitudes, umbral_angular)
         promedio_lon = estabilizacion_modificada(longitudes, umbral_angular)
         promedio_alt = estabilizacion_modificada(alturas, umbral_metrico)
+        # Convierto los valores angulares a grados minutos y segundos
         promedio_lat_dms = dd2dms(promedio_lat)
         promedio_lon_dms = dd2dms(promedio_lon)
-        
-        # Representación gráfica de las coordenadas y promedios
+        # Representación gráfica de las coordenadas y promedios en tres graficas distintas
         fig, ax = plt.subplots(3, 1, figsize=(8, 12), sharex=True)
-    
         plot_coords(fig, ax, tiempos, latitudes, 'N-S', promedio_lat, 0, coord_ticks)
         plot_coords(fig, ax, tiempos, longitudes, 'E-W', promedio_lon, 1, coord_ticks)
         plot_coords(fig, ax, tiempos, alturas, 'U-D [m]', promedio_alt, 2)
@@ -435,11 +417,11 @@ for filename in os.listdir(input_folder):
         plt.show()
         plt.tight_layout()
         
+        # Guardo los promedios, las desviaciones y los errores de las desviaciones en un archivo.
         with open(os.path.join(ruta_resultados, "posiciones.txt"), "w") as archivo_resultados:
             archivo_resultados.write("#  UTC            latitude            longitude            height(m)    sdn(m)   sde(m)   sdu(m)   sdne(m)  sdeu(m)  sdue(m)\n")
             archivo_resultados.write(f"{fecha}        {promedio_lat_dms[0]:.0f}°{promedio_lat_dms[1]:.0f}'{promedio_lat_dms[2]:.5f}\"     {promedio_lon_dms[0]:.0f}°{promedio_lon_dms[1]:.0f}'{promedio_lon_dms[2]:.5f}\"    {promedio_alt:.4f}      {sdn:.4f}   {sde:.4f}   {sdu:.4f}   {sdne:.4f}  {sdeu:.4f}   {sdue:.4f}\n")
 
-    
     # Gráfica en 3D
     latitudes_deg, latitudes_min, latitudes_sec = zip(*[dd2dms(lat) for lat in latitudes])
     longitudes_deg, longitudes_min, longitudes_sec = zip(*[dd2dms(lon) for lon in longitudes])
@@ -447,26 +429,22 @@ for filename in os.listdir(input_folder):
     latitudes_dms = [(lat_d, lat_m, lat_s) for lat_d, lat_m, lat_s in zip(latitudes_deg, latitudes_min, latitudes_sec)]
     longitudes_dms = [(lon_d, lon_m, lon_s) for lon_d, lon_m, lon_s in zip(longitudes_deg, longitudes_min, longitudes_sec)]
 
+    # Funcion para graficar los datos en 3D
     def plot_3d_path(latitudes, longitudes, alturas, promedio_lat, promedio_lon, promedio_alt):
         fig = plt.figure(figsize=(8, 8))
         ax = fig.add_subplot(111, projection='3d')
-    
         latitudes_decimal = [dms2dd(*lat) for lat in latitudes]
         longitudes_decimal = [dms2dd(*lon) for lon in longitudes]
-    
         ax.plot(longitudes_decimal, latitudes_decimal, alturas, marker='o', linestyle='-', linewidth=0.7, markersize=2)
-
         # Agregar el punto promedio al gráfico 3D
         ax.scatter(promedio_lon, promedio_lat, promedio_alt, c='r', marker='o', s=100, label="Promedio")
-
         ax.set_xlabel('Longitud [°]')
         ax.set_ylabel('Latitud [°]')
         ax.set_zlabel('Altura [m]')
         ax.legend()  # Agregar la leyenda
-
         plt.show()
 
-    # Llamar a la función con los valores promedio
+    # Ploteo la gráfica en 3D
     plot_3d_path(latitudes_dms, longitudes_dms, alturas, promedio_lat, promedio_lon, promedio_alt)
 
     
